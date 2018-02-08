5f6724a20a82a3368c43cceb9e346523cf041047
marb61a <marb61a@gmail.com>
2018-02-08 13:45:25 +0000

    Added new activity and drawable images

--- app/src/main/java/com/example/martin/android_weather/WeatherController.java
+++ app/src/main/java/com/example/martin/android_weather/WeatherController.java
@@ -1,9 +1,22 @@
 package com.example.martin.android_weather;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WeatherController extends AppCompatActivity {
+    // Member Variables:
+    boolean mUseLocation = true;
+    TextView mCityLabel;
+    ImageView mWeatherImage;
+    TextView mTemperatureLabel;
+
+    // Declaring a LocationManager and a LocationListener here:
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/martin/android_weather/WeatherDataModel.java
@@ -0,0 +1,14 @@
+package com.example.martin.android_weather;
+
+/**
+ * Created by martin on 08/02/18.
+ */
+
+public class WeatherDataModel {
+    // Member variables that will hold relevant weather information
+    private String mTemperature;
+    private String mCity;
+    private String mIconName;
+    private int mCondition;
+
+}
