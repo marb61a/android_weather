09130df3b63b9fb86ff1ff7b6bbc7b7f4073468a
marb61a <marb61a@gmail.com>
2018-02-13 13:19:35 +0000

    Updated change city controller

--- app/src/main/java/com/example/martin/android_weather/ChangeCityController.java
+++ app/src/main/java/com/example/martin/android_weather/ChangeCityController.java
@@ -1,8 +1,40 @@
 package com.example.martin.android_weather;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 /**
  * Created by martin on 13/02/18.
  */
 
-public class ChangeCityController {
+public class ChangeCityController extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+
+        final EditText editTextField = findViewById(R.id.queryET);
+        ImageButton backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go back and destroy the ChangeCityController
+                finish();
+            }
+        });
+
+        // EditTexts can have listeners for keyboard presses like hitting the enter key.
+        editTextField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                return false;
+            }
+        });
+    }
 }
