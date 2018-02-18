83ff7bb5a1ef48da193d91b19e38e06049d16147
marb61a <marb61a@gmail.com>
2018-02-18 16:57:49 +0000

    added location methods to weathercontroller

--- app/src/main/java/com/example/martin/android_weather/WeatherController.java
+++ app/src/main/java/com/example/martin/android_weather/WeatherController.java
@@ -1,13 +1,24 @@
 package com.example.martin.android_weather;
 
+import android.content.Context;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WeatherController extends AppCompatActivity {
+
+    // Avoids typing 'Clima' in all the logs by putting this in a constant here.
+    final String LOGCAT_TAG = "Clima";
+
+    // Setting the LOCATION_PROVIDER here, using this with GPS_Provider is good when testing
+    // on an emulator, when using a physical device use LocationManager.NETWORK_PROVIDER
+    final String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
+
     // Member Variables:
     boolean mUseLocation = true;
     TextView mCityLabel;
@@ -22,5 +33,42 @@ public class WeatherController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_controller_layout);
+    }
+
+    // onResume() lifecycle callback:
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(LOGCAT_TAG, "onResume() called");
+        if(mUseLocation) getWeatherForCurrentLocation();
+    }
+
+    // Location Listener callbacks here, when the location has changed.
+    private void getWeatherForCurrentLocation(){
+        Log.d(LOGCAT_TAG, "Getting weather for current location");
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        mLocationListener = new LocationListener(){
+            @Override
+            public void onLocationChanged(Location location){
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
     }
 }
